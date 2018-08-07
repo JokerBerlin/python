a073255dd4a35aecfe5b374713ce8f79856b7659
JokerBerlin <kenny.flores.27@unsch.edu.pe>
2018-08-06 20:19:51 -0500

    este es el segundo cambio

--- prueba/settings.py
+++ prueba/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 #esto es un commit
+#esto es el segundo cambio
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
