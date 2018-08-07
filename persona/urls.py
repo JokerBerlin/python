bbb3bbbb65c3ffdf5ca3a99ade50a6f58d3b33a7
JokerBerlin <kenny.flores.27@unsch.edu.pe>
2018-08-07 17:59:25 -0500

    dividiendo templates en carpetas

--- persona/urls.py
+++ persona/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 urlpatterns = [
     path(r'', views.hola, name="hola"),
-	path(r'ultimos_libros/', views.ultimos_libros, name="ultimos_libros")
+	path(r'ultimos_libros/', views.ultimos_libros, name="ultimos_libros"),
+    path(r'hora/',views.fecha_actual, name="hora"),
+    #path(r'fechas/\d/', views.horas_adelante),
+    re_path(r'fecha/(\d{1,2})/$', views.horas_adelante),
 ]
--- persona/views.py
+++ persona/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from .models import Libro
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+import datetime
+from django.template import loader, Context
+
 # Create your views here.
 
 def hola(request):
     return HttpResponse("Hola mundo")
 
+def fecha_actual(request):
+    ahora = datetime.datetime.now()
+    template = loader.get_template('tiempo/hora.html')
+    context = {
+        'fecha_actual':ahora
+    }
+    return HttpResponse(template.render(context,request))
+    #primera linea
+    #return render(request, 'hora.html', context)
+
 def ultimos_libros(request):
     lista_libros = Libro.objects.order_by('-fecha')[:10]
-    return render(request,'ultimos_libros.html', {'lista_libros': lista_libros} )
+    return render(request,'libro/ultimos_libros.html', {'lista_libros': lista_libros} )
+
+def horas_adelante(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now()+datetime.timedelta(hours=offset)
+    html = "<html><body><h1>En %s hora(s), seran:</h1> <h3>%s</h3></body></html>" % (offset, dt)
+    return HttpResponse(html)
