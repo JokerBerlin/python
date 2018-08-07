dcdfaed8b2d6a333d254df95fc4895a6d85f8367
JokerBerlin <kenny.flores.27@unsch.edu.pe>
2018-08-06 20:23:19 -0500

    terce y ultimo cambio

--- prueba/settings.py
+++ prueba/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
+
+#este es el ultimo cambio de hoy
 #esto es un commit
 #esto es el segundo cambio
 import os
